ec8328d0cc899519cd3e6cbf824486c582c7df47
Vishal Thakur <vishalsanjivthakur@gmail.com>
2023-04-25 21:53:00 +0530

    Fixed header image alignment

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,17 @@
-navBar = document.querySelector(".nav");
-navLinkContainer = document.querySelector(".nav-link-container");
-navLinkButton = document.querySelector(".nav-link-button");
+const navBar = document.querySelector(".nav");
+const navLinkContainer = document.querySelector(".nav-link-container");
+const navLinkButton = document.querySelector(".nav-link-button");
+
+document.addEventListener("scroll", (e) => {
+    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    console.log(scrollTop)
+    if(scrollTop > 0) {
+      navBar.classList.add("nav-shadow");
+    }
+    else {
+      navBar.classList.remove("nav-shadow");
+    }
+  })
 
 navLinkButton.onclick = () => {
   navBar.classList.toggle("nav-active");
