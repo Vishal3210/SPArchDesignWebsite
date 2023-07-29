72b5013bb602f38c8bab38f6de5f37af407ee22b
Vishal Thakur <vishalsanjivthakur@gmail.com>
2023-07-29 22:05:06 +0530

    Services slideshow complete

--- assets/js/ourServicesPrevNext.js
+++ assets/js/ourServicesPrevNext.js
@@ -3,16 +3,82 @@ const nextButton = document.querySelector(".our-services-navigation-button.next"
 
 const ourServicesCards = document.querySelectorAll(".our-services-main-content-card");
 
+const navigationButtons = document.querySelectorAll(".our-services-navigation-button");
+const pausePlayButtons = document.querySelectorAll(".our-services-button");
+
 const displayServicesCard = (idx) => {
+    ourServicesCards[idx].classList.add('active');
+}
+
+let cardInterval;
+const setCardInterval = () => {
+    cardInterval = setInterval(autoServicesCardSlider, 5000);
+}
+
+const clearCardInterval = () => {
+    clearInterval(cardInterval);
+}
+
+let paused = false;
+
+const setPausePlayState = () => {
+    if (paused) {
+        pausePlayButtons[0].classList.remove('active');
+        pausePlayButtons[1].classList.add('active');
+    }
+    else {
+        pausePlayButtons[0].classList.add('active');
+        pausePlayButtons[1].classList.remove('active');    
+    }
+}
+
+const autoServicesCardSlider = () => {
     ourServicesCards.forEach((card, index) => {
-        card.classList.remove('active');
-        if (idx == index) {
-            console.log(index);
-            card.classList.add('active');
+        if(card.classList.contains('active')) {
+            let tempIndex = index + 1 >= ourServicesCards.length ? 0 : index + 1;
+            card.classList.remove('active');
+            cardShowTimeout = setTimeout(displayServicesCard, 500, tempIndex);
+            return;
         }
     });
 }
 
-const autoServicesCardSlider = () => {
-    
-}
+pausePlayButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('pause')) {
+            clearCardInterval();
+            paused = true;
+            setPausePlayState();
+        }
+        else {
+            setCardInterval();
+            paused = false;
+            setPausePlayState();
+        }
+    });
+});
+
+navigationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        ourServicesCards.forEach((card, index) => {
+            if(card.classList.contains('active')) {
+                let tempIndex
+                if (button.classList.contains('prev')) {
+                    tempIndex = index - 1 < 0 ? ourServicesCards.length - 1 : index - 1;
+                }
+                else {
+                    tempIndex = index + 1 >= ourServicesCards.length ? 0 : index + 1;  
+                }
+                card.classList.remove('active');
+                cardShowTimeout = setTimeout(displayServicesCard, 500, tempIndex);
+                if (!paused) {
+                    clearCardInterval();
+                    setCardInterval();
+                }
+                return;
+            }
+        });
+    });
+});
+
+setCardInterval();
